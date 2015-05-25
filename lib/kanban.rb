79764dd2d4a1649b4c889e345d2460881f9907af
Chris Olstrom <chris@olstrom.com>
2015-05-25 12:52:11 -0700

    lib/kanban: Named queues

--- lib/kanban.rb
+++ lib/kanban.rb
@@ -1,9 +1,10 @@
 module Kanban
   class Backlog
-    attr_reader :namespace
+    attr_reader :namespace, :queue
 
     def initialize(**options)
       @namespace = options.fetch :namespace, 'default'
+      @queue = "#{@namespace}:#{options.fetch :queue, 'tasks'}"
     end
   end
 end
