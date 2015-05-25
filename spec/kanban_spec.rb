78e869da9ff63f50b9ff79d10203f69675e246e0
Chris Olstrom <chris@olstrom.com>
2015-05-25 12:01:22 -0700

    spec/kanban: Create Backlog in setup block rather than test.

--- spec/kanban_spec.rb
+++ spec/kanban_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Backlog" do
+  before do
+    @backlog = Kanban::Backlog.new
+  end
+
   it "instantiates without error" do
-    Kanban::Backlog.new.should be_an_instance_of(Kanban::Backlog)
+    @backlog.should be_an_instance_of(Kanban::Backlog)
   end
 end
