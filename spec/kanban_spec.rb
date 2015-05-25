620f4b96bdc3b9a2f983466d4f4cfb3fdeb10f20
Chris Olstrom <chris@olstrom.com>
2015-05-25 12:00:12 -0700

    spec/kanban: Fall back to rspec 2.x syntax

--- spec/kanban_spec.rb
+++ spec/kanban_spec.rb
@@ -2,6 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Backlog" do
   it "instantiates without error" do
-    expect(Kanban::Backlog.new).to be_an_instance_of(Kanban::Backlog)
+    Kanban::Backlog.new.should be_an_instance_of(Kanban::Backlog)
   end
 end
