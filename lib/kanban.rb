792a1eaaf8567f00bed637567b74efb58b2e881c
Chris Olstrom <chris@olstrom.com>
2015-05-25 12:54:15 -0700

    lib/kanban: Named item keys

--- lib/kanban.rb
+++ lib/kanban.rb
@@ -1,10 +1,11 @@
 module Kanban
   class Backlog
-    attr_reader :namespace, :queue
+    attr_reader :namespace, :queue, :item
 
     def initialize(**options)
       @namespace = options.fetch :namespace, 'default'
       @queue = "#{@namespace}:#{options.fetch :queue, 'tasks'}"
+      @item = "#{@namespace}:#{options.fetch :item, 'task'}"
     end
   end
 end
