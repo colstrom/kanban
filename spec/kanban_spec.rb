a9fe25bc680be0835ff80eacb27bba0ea80a9771
Chris Olstrom <chris@olstrom.com>
2015-05-25 11:22:28 -0700

    spec/kanban: Create Backlog

--- spec/kanban_spec.rb
+++ spec/kanban_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Kanban" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Backlog" do
+  it "instantiates without error" do
+    expect(Kanban::Backlog.new).to be_an_instance_of(Kanban::Backlog)
   end
 end
