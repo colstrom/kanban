91518cabf42b9dea8023bc3fc0d6846cdb99a114
Chris Olstrom <chris@olstrom.com>
2015-05-25 23:36:35 -0700

    lib/hash_safety: Space missing after comma

--- lib/kanban/hash_safety.rb
+++ lib/kanban/hash_safety.rb
@@ -5,7 +5,7 @@ module HashSafety
     end
 
     def with_string_keys
-      each_with_object({}) do |(key,value),hash|
+      each_with_object({}) do |(key, value), hash|
         hash[key.to_s] = value
         hash
       end
