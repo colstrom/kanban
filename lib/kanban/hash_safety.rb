d67c7332534946e7c3de962c3f3343eb5ab70a7d
Chris Olstrom <chris@olstrom.com>
2015-05-25 22:24:59 -0700

    lib/hash_safety: Provide safe copy of hash

--- lib/kanban/hash_safety.rb
+++ lib/kanban/hash_safety.rb
@@ -3,5 +3,12 @@ module HashSafety
     def keys_contain_symbols?
       self.keys.select { |key| key.is_a? Symbol }.size > 0
     end
+
+    def with_string_keys
+      self.each_with_object({}) do |(key,value),hash|
+        hash[key.to_s] = value
+        hash
+      end
+    end
   end
 end
