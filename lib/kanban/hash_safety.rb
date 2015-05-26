73e82b3d0e32941abcc74ce6db4db14f473a759a
Chris Olstrom <chris@olstrom.com>
2015-05-25 23:33:48 -0700

    lib/hash_safety: Removed redundant self

--- lib/kanban/hash_safety.rb
+++ lib/kanban/hash_safety.rb
@@ -1,11 +1,11 @@
 module HashSafety
   refine Hash do
     def keys_contain_symbols?
-      self.keys.select { |key| key.is_a? Symbol }.size > 0
+      keys.select { |key| key.is_a? Symbol }.size > 0
     end
 
     def with_string_keys
-      self.each_with_object({}) do |(key,value),hash|
+      each_with_object({}) do |(key,value),hash|
         hash[key.to_s] = value
         hash
       end
