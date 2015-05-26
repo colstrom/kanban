e57abc7a5a147dd9f5d2b0b378d593869b4ea619
Chris Olstrom <chris@olstrom.com>
2015-05-25 22:24:31 -0700

    spec/hash_safety: Provide safe copy of hash

--- spec/kanban/hash_safety_spec.rb
+++ spec/kanban/hash_safety_spec.rb
@@ -11,4 +11,10 @@ describe "HashSafety" do
   it 'should detect symbols in hash keys' do
     expect(@symbol_hash.keys_contain_symbols?).to be true
   end
+
+  it 'should provide an alternate hash with string keys' do
+    safe = @symbol_hash.with_string_keys
+    expect(safe).to eq @string_hash
+    expect(safe.keys_contain_symbols?).to be false
+  end
 end
